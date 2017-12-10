720feaf9c6e779f37688d3c9b3d08830e951a7f6
Norbi <ksznorbi@gmail.com>
2017-12-10 22:48:54 +0100

    added checkpoints

--- lyrics.py
+++ lyrics.py
@@ -3,6 +3,8 @@
 import numpy as np
 from utils import CharEncoder, read_file, persist_model, get_chunk
 from sequentials import get_classifier_1lstm_short_mem
+from keras.callbacks import ModelCheckpoint, TensorBoard
+import os
 
 data = read_file('dataset/30y.txt')
 encoder = CharEncoder()
@@ -12,9 +14,19 @@ chunks = enumerate(get_chunk(data, 100, 100))
 _, (train_X, train_y) = next(chunks)
 classifier = get_classifier_1lstm_short_mem(train_X, train_y)
 
+
+# define the checkpoint
+filepath=os.path.join("checkpoints","weights-improvement-{epoch:02d}-{loss:.4f}.hdf5")
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+tensorboard = TensorBoard(log_dir='./logs', histogram_freq=250, write_graph=True, write_images=False)
+callbacks_list = [checkpoint, tensorboard]
+
+# reseting the generator
+chunks = enumerate(get_chunk(data, 100, 100))
+
 for i, (train_X, train_y) in chunks:
     print('%d. chunk is being trained on.'% i)
-    classifier.fit(train_X, train_y, epochs=45, batch_size=250, validation_split=0.2)
+    classifier.fit(train_X, train_y, epochs=10, batch_size=250, validation_split=0.2, callbacks=callbacks_list)
 
 
 sample = '''A buli íze a számban
