949cc941e78ccb8e88dc677ef1d35db5e18cb4b3
Norbert Kis-Szabo <ksznorbi@gmail.com>
2018-02-05 15:27:24 +0100

    Readded tensorboard, encoders are now saved, added transform feature to encoder (like lowercase)

--- lyrics.py
+++ lyrics.py
@@ -1,34 +1,40 @@
 # -*- coding: utf-8 -*-
-
-import numpy as np
-from utils import CharEncoder, read_file, persist_model, load_model, DataChunk, TrainLogger
+from utils import *
 from sequentials import get_classifier_1lstm_short_mem, get_classifier_2lstm_medium_mem, get_classifier_1lstm_long_mem
+from keras.callbacks import TensorBoard
 import math
 
 ARTIST = 'halott-penz'
-EPOCHS = 600
+EPOCHS = 10
+PATIENCE_LIMIT = 25
 data = read_file('dataset/%s.txt' % ARTIST)
-encoder = CharEncoder() #TODO save encoder as well
-data = encoder.fit_transform(data)
-DATA_SLICE = int(len(data) / 10)
+DATA_SLICE = len(data) // 10
+
+log = TrainLogger('train-log-%s.csv' % ARTIST)
+
+TENSOR_LOGGER = './tf-logs'
+tensorboard = TensorBoard(TENSOR_LOGGER)
+train_log_per_batch_names = ['train_batch_loss', 'train_batch_accuracy']
+train_log_per_epoch_names = ['train_epoch_loss', 'train_epoch_accuracy']
+val_log_names = ['val_loss', 'val_accuracy']
+test_log_names = ['test_loss', 'test_accuracy']
+
+encoder = load_or_create_encoder(ARTIST, ENCODER_FORMAT_LOWERCASE, data)
+data = encoder.transform(data)
 # Split data for testing and validating purposes
 val_data = data[0: DATA_SLICE]
 test_data = data[DATA_SLICE:2*DATA_SLICE]
 data = data[DATA_SLICE:]
 chunk = DataChunk(data, 100, 300)
 test_chunk = DataChunk(test_data, 100, 300)
-classifier = get_classifier_1lstm_long_mem(*chunk.get_dummy())
 
-log = TrainLogger('train-log-%s.csv' % ARTIST)
-# define the checkpoint
-# filepath=os.path.join("checkpoints", "weights-improvement-{epoch:02d}-{loss:.4f}.hdf5")
-# checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-# tensorboard = TensorBoard(log_dir='./logs', histogram_freq=250, write_graph=True, write_images=False)
-# callbacks_list = [checkpoint, tensorboard]
+classifier = get_classifier_1lstm_short_mem(*chunk.get_dummy())
+tensorboard.set_model(classifier)
+
 min_loss = math.inf
 patience = 0
-patience_limit = 25
 best_model_name = None
+global_steps = 0
 for epoch in range(EPOCHS):
     print('\n[%d]Epoch %d/%d' % (epoch + 1, epoch + 1, EPOCHS))
     chunk = DataChunk(data, 100, 300)
@@ -37,8 +43,10 @@ for epoch in range(EPOCHS):
     for i, (train_X, train_y) in enumerate(iter(chunk)):
         loss, acc = classifier.train_on_batch(train_X, train_y)
         print('[%d]Batch %d: loss = %f, acc = %f' % (epoch + 1, i + 1, loss, acc))
+        write_log_to_board(tensorboard, train_log_per_batch_names, (loss, acc), global_steps)
         losses.append(loss)
         accs.append(acc)
+        global_steps += 1
 
     for (val_X, val_y) in iter(val_chunk):
         val_loss, val_acc = classifier.test_on_batch(val_X, val_y)
@@ -46,9 +54,14 @@ for epoch in range(EPOCHS):
         v_accs.append(val_acc)
 
     val_loss_avg = np.average(v_losses)
-    print('[%d]FINISHING EPOCH.. val_loss = %f, val_acc = %f' % (epoch + 1, val_loss_avg, np.average(v_accs)))
+    val_acc_avg = np.average(v_accs)
+    train_loss_avg = np.average(losses)
+    train_acc_avg = np.average(accs)
+    write_log_to_board(tensorboard, val_log_names, (val_loss_avg, val_acc_avg), global_steps)
+    write_log_to_board(tensorboard, train_log_per_epoch_names, (train_loss_avg, train_acc_avg), global_steps)
+    print('[%d]FINISHING EPOCH.. val_loss = %f, val_acc = %f' % (epoch + 1, val_loss_avg, val_acc_avg))
 
-    log.add_entry(np.average(losses), np.average(accs), val_loss_avg, np.average(v_accs))
+    log.add_entry(train_loss_avg, train_acc_avg, val_loss_avg, val_acc_avg)
 
     if val_loss_avg <= min_loss:
         min_loss = val_loss_avg
@@ -56,8 +69,8 @@ for epoch in range(EPOCHS):
         best_model_name = '%s_e_%d_l_%.4f' % (ARTIST, epoch, min_loss)
         persist_model(classifier, best_model_name)
         print('[%d]New best model for validation set found.. val_loss = %f, val_acc = %f' % (epoch + 1, val_loss_avg, np.average(v_accs)))
-    elif patience >= patience_limit:
-        print('[%d]Patience limit (%d) reached stopping iteration. Best validation loss found was: %f' % (epoch + 1, patience_limit, min_loss))
+    elif patience >= PATIENCE_LIMIT:
+        print('[%d]Patience limit (%d) reached stopping iteration. Best validation loss found was: %f' % (epoch + 1, PATIENCE_LIMIT, min_loss))
         break
     else:
         patience += 1
@@ -66,8 +79,9 @@ classifier = load_model(best_model_name)
 classifier.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=['accuracy'])
 
 t_losses, t_accs = [], []
-for (test_X, test_y) in iter(test_chunk):
+for i, (test_X, test_y) in enumerate(iter(test_chunk)):
     test_loss, test_acc = classifier.test_on_batch(test_X, test_y)
+    write_log_to_board(tensorboard, test_log_names, (test_loss, test_acc), i)
     t_losses.append(test_loss)
     t_accs.append(test_acc)
 
--- utils.py
+++ utils.py
@@ -7,17 +7,20 @@ Created on Sat Nov 25 17:15:11 2017
 @author: Norbi
 """
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
+#for pickling lambdas
+import dill as pickle
 import numpy as np
 import os
+import tensorflow as tf
+
+MODELS_DIR = 'models'
+LOG_DIR = 'train_log'
+ENCODERS_DIR = 'encoders'
 
 def read_file(file_content):
     with open(file_content, encoding='utf-8') as file_ref:
         content = file_ref.read()
     return content
-
-
-MODELS_DIR = 'models'
 
 
 def persist_model(model, name):
@@ -28,7 +31,8 @@ def persist_model(model, name):
         json.write(serialized)
     
     model.save_weights(os.path.join("models", name+".h5"))
-    
+
+
 def load_model(name):
     from keras.models import model_from_json
     loaded = open(os.path.join("models", name+".json"), 'r')
@@ -38,6 +42,33 @@ def load_model(name):
     model.load_weights(os.path.join("models", name+".h5"))
     
     return model
+
+
+def persist_object(obj, name):
+    file_name = '%s.pickle' % name
+    if os.path.exists(file_name):
+        raise FileExistsError
+    with open(file_name, 'wb') as file_stream:
+        pickle.dump(obj, file_stream)
+
+
+def load_object(name):
+    with open('%s.pickle' % name, 'rb') as file_stream:
+        return pickle.load(file_stream)
+
+
+def write_log_to_board(tensorboard_callback, names, logs, batch_no):
+    """
+    [Source](https://gist.github.com/joelthchao/ef6caa586b647c3c032a4f84d52e3a11#file-demo-py-L24)
+    """
+    for name, value in zip(names, logs):
+        summary = tf.Summary()
+        summary_value = summary.value.add()
+        summary_value.simple_value = value
+        summary_value.tag = name
+        tensorboard_callback.writer.add_summary(summary, batch_no)
+        tensorboard_callback.writer.flush()
+
 
 class DataChunk:
     
@@ -89,16 +120,28 @@ class DataChunk:
         return dummy_X, dummy_y
 
 
+ENCODER_FORMAT_LOWERCASE = {'lowercase': lambda t: t.lower()}
+
+
 class CharEncoder:
     
-    def __init__(self):
+    def __init__(self, formaters=dict()):
         self.onehot = OneHotEncoder()
         self._RARE = '<RARE_CHAR>'
-        
+        self._vocab = dict()
+        self._formaters = formaters
+
+    def _format(self, y):
+        for _, formater in self._formaters.items():
+            y = formater(y)
+        return y
+
     def transform(self, y):
+        y = self._format(y)
         return np.array([self._vocab[ch] if ch in self._vocab else self._vocab[self._RARE].flatten() for ch in y])
     
     def fit(self, y):
+        y = self._format(y)
         vocab = sorted(list(set(y)))
         vocab.append(self._RARE)
         self._vocab = dict((c,i) for i, c in enumerate(vocab))
@@ -106,20 +149,44 @@ class CharEncoder:
         self.onehot.fit(np.array(list(self._vocab.values())).reshape(-1, 1))
         for key, value in self._vocab.items():
             self._vocab[key] = self.onehot.transform(value).toarray().flatten()
-        
     
     def fit_transform(self, y):
         self.fit(y)
+        y = self._format(y)
         return self.transform(y)
     
     def inverse_transform(self, y):
         rev_vocab = {self._invert_onehot(v): k for k, v in self._vocab.items()}
         return ''.join([rev_vocab[self._invert_onehot(i)] for i in y])
 
-    def _invert_onehot(self, encoded):
-        return np.where(encoded==1)[0][0]
+    @staticmethod
+    def _invert_onehot(encoded):
+        return np.where(encoded == 1)[0][0]
 
-LOG_DIR = 'train_log'
+    def get_formaters_str(self):
+        return get_formaters_str(self._formaters)
+
+
+def get_formaters_str(format_dict):
+    """
+    with the same formaters you should get the same string
+    :return: formater identifier
+    """
+    return ''.join(sorted(list(map(lambda key: key[0], [key for key, _ in format_dict.items()]))))
+
+
+def load_or_create_encoder(artist, formatter, data):
+    filename = '%s/%s_encoder_%s' % (ENCODERS_DIR, artist, get_formaters_str(formatter))
+    try:
+        encoder = load_object(filename)
+    except FileNotFoundError:
+        encoder = CharEncoder(ENCODER_FORMAT_LOWERCASE)
+        encoder.fit(data)
+        if not os.path.exists(ENCODERS_DIR):
+            os.mkdir(ENCODERS_DIR)
+        persist_object(encoder, filename)
+    return encoder
+
 
 class TrainLogger(object):
     '''
